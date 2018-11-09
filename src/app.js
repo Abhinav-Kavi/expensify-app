0e4d952ce83be29a14cabfd8a858c69201d24625
Kavi <abkavi@deloitte.com>
2018-11-09 15:29:40 +0530

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';  
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-import { setTimeout } from 'core-js';
 import 'react-dates/lib/css/_datepicker.css';
 
 
